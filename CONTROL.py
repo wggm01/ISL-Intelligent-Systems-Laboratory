0b634e9a3e660fa5834e1d3feea0cc52f7224e13
wggm01 <wggm01@gmail.com>
2018-03-03 14:38:34 -0500

    CONTROL.py

--- CONTROL.py
+++ CONTROL.py
@@ -55,12 +55,12 @@ while instruccion == 'y':
             lat = -lat
         if  gps_sentences_fields[6] == "W":
             longi = -longi
-#-----------Elección de region----------------------------
+
         if region == 3:
             region =2
 
 
-#-----------Elección de region----------------------------
+
 #-----------Calculo de distancia usando Harversine----------------------------
         if region == 1 :
             #Region 1
@@ -76,21 +76,21 @@ while instruccion == 'y':
             print d
 #-----------Calculo de distancia usando Harversine----------------------------
         if region == 2 :
-        #Region 2
-        latref2=9.023149167
-        lonref2=-79.53156583
-        radius = 6371 # km
-        dlat = math.radians(latref2-lat)
-        dlon = math.radians(longref2-longi)
-        a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat)) \
-        * math.cos(math.radians(latref2)) * math.sin(dlon/2) * math.sin(dlon/2)
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        d = int((radius*c)*1000)
-        print d
+            #Region 2
+            latref2=9.023149167
+            lonref2=-79.53156583
+            radius = 6371 # km
+            dlat = math.radians(latref2-lat)
+            dlon = math.radians(longref2-longi)
+            a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat)) \
+            * math.cos(math.radians(latref2)) * math.sin(dlon/2) * math.sin(dlon/2)
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            d = int((radius*c)*1000)
+            print d
 #------------------------------------------------------------------------------
 #--------comando = instrucion enviada al arduino---------------------------
 #----Los limites se definen mediante las coordenadas de referencias-------
-	   if latref ==  and longref ==  : # 9.02318033 -79.53151733 original
+	    if latref ==9.04525  and longref ==-79.40719  : # 9.02318033 -79.53151733 original
             #Limites para las region1
     		"""min1 = 34
     		max1 = 6  # min<--------------Region1--------------------------->max
@@ -100,10 +100,10 @@ while instruccion == 'y':
             min1=80
             max1=4
             min2=4
-    		print("Region 1")
+    	    print("Region 1")
 
 
-	   if latref2 ==  and longref2 ==  : #9.023149167 -79.53156583 original
+	    if latref2 ==9.04485  and longref2 ==-79.40695  : #9.023149167 -79.53156583 original
             #Limite para Region 2
     		"""min1 = 7
     		max1 = 2 # min<--------------Region2--------------------------->max
@@ -113,23 +113,22 @@ while instruccion == 'y':
             min1=80
             max1=4
             min3=4
+    	    print("Region 2")
 
-    		print("Region 2")
-
-       if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
+        if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
             #arduino.write(Forward) #Mandar un comando hacia Arduino
             bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
             bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
             print("Moviendo")
 
-       if d <= min2:#Establece cuando curvara
+        if d <= min2:#Establece cuando curvara
             #arduino.write(Turn)#Mandar un comando hacia Arduino
             bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
             bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
             print("Curvando")
             time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
             region2=2
-       if d <= min3:#Establece cuando curvara
+        if d <= min3:#Establece cuando curvara
             #arduino.write(Turn)#Mandar un comando hacia Arduino
             bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
             bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
