142b9d6162764d08e257ec7aa81b4d6a9ed00d70
wggm01 <wggm01@gmail.com>
2018-03-02 19:48:50 -0500

    Arreglos para usar comunicación i2c
    AGREGE LA LIBRERIA SMBUS, LAS DIRECCIONES DE LOS ESCLAVOS Y CAMBIA LAS INSTRUCCIONES PARA EL ENVIO DE LAS INSTRUCCIONES.

--- CONTROL.py
+++ CONTROL.py
@@ -1,17 +1,30 @@
-import serial
+#1-hacia alfrente
+#2-hacia atras
+#3-Dobla hacia la izquierda
+#4-Dobla hacia la derecha
+#5-Detener
+
+#-------------Librerias--------------------------
+#import serial
 import time
-import machinarie
+#import machinarie
 import math
-
-Forward='F'
-Turn='T'
-Stop = 'S'
+import smbus
+#-----------Variables principales-----------------
+Forward=1
+Turn= 4
+Stop = 5
 delay = 10 #Este valor hay que estimarlo al ojo.
+slaveAddress2 = 0x40 #MotorIzquierdo
+slaveAddress1 = 0x50 #MotorDerecho
+bus = smbus.SMBus(1) #Bus por el cual se comunican
+#-------------------------------------------------
 
-#Lectura del puerto serial
+#-----------Conecciones----------------------------
 gps = serial.Serial("/dev/ttyACM0", baudrate = 9600)
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
+#arduino = serial.Serial('/dev/ttyUSB0', 9600)
 #gps = serial.Serial('COM3', 9600)
+#--------------------------------------------------
 
 
 instruccion = raw_input("When u're ready press y to start  \n ")
@@ -34,7 +47,7 @@ while instruccion == 'y':
             lat = -lat
         if  gps_sentences_fields[6] == "W":
             longi = -longi
-         #Calculo para saber cuanto falta para terminar region 1
+#-----------Calculo de distancia usando Harversine----------------------------
         latref =9.02318033
         longref = -79.53151733 #region1
         radius = 6371 # km
@@ -45,33 +58,34 @@ while instruccion == 'y':
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
         d = int((radius*c)*1000)
         print d
-        #comando = instrucion enviada al arduino
+#------------------------------------------------------------------------------
+#--------comando = instrucion enviada al arduino---------------------------
+#----Los limites se definen mediante las coordenadas de referencias-------
 	if latref == 9.02318033 and longref == -79.53151733 :
-		min1 = 34#se puede quedar asi
-		max1 = 6#cambiar
-		min2 = 6 #Minimo y max para cuando inicia la region
+        #Limites para las region1
+		min1 = 34
+		max1 = 6  # min<--------------Region--------------------------->max
+		min2 = 6
 		#max2 =25
 		print("Region 1")
 	if latref == 9.023149167 and longref == -79.53156583 :
-		min1 = 7#se puede quedar asi
-		max1 = 2#cambiar
-		min2 = 1 #Minimo y max para cuando termina la region
+        #Limite para Region 2
+		min1 = 7
+		max1 = 2 # min<--------------Region--------------------------->max
+		min2 = 1
 		#max2 = 8
 		print("Region 2")
-        if d < min1 and d >= max1: #Establecer limites manualmente para mover
-               arduino.write(Forward) #Mandar un comando hacia Arduino
+        if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
+               #arduino.write(Forward) #Mandar un comando hacia Arduino
+               bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
+               bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
                print("Moviendo")
-        
-        if d <= min2:#Establecer limites manualmente para cuando esta terminando la region  
-               arduino.write(Turn)#Mandar un comando hacia Arduino
-               print("Curvo")
-            #time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
-               latref=9.023149167
-               lonref=-79.53156583
-        
-	   
-           
-        
-    
 
-   
+        if d <= min2:#Establece cuando curvara
+               #arduino.write(Turn)#Mandar un comando hacia Arduino
+               bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
+               bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
+               print("Curvand")
+            #time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
+               latref=9.023149167 #Cambio de la referencia para la region2
+               lonref=-79.53156583
