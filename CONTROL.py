4b81fb07a707d9914b47372313a61340a864534f
wggm01 <wggm01@gmail.com>
2018-03-03 13:46:42 -0500

    CONTROL.py

--- CONTROL.py
+++ CONTROL.py
@@ -18,6 +18,13 @@ delay = 10 #Este valor hay que estimarlo al ojo.
 slaveAddress2 = 0x40 #MotorIzquierdo
 slaveAddress1 = 0x50 #MotorDerecho
 bus = smbus.SMBus(1) #Bus por el cual se comunican
+#Region 1
+#latref =9.02318033
+#longref = -79.53151733
+#Region 2
+#latref2=9.023149167
+#lonref2=-79.53156583
+region = 0
 #-------------------------------------------------
 
 #-----------Conecciones----------------------------
@@ -27,9 +34,10 @@ gps = serial.Serial("/dev/ttyACM0", baudrate = 9600)
 #--------------------------------------------------
 
 
-instruccion = raw_input("When u're ready press y to start  \n ")
+instruccion = raw_input("Cuando este listo presione y para iniciar \n ")
 #loop para que muestre cada lectura que recibe el gps
 while instruccion == 'y':
+    region = 1 + region2
     gps_sentece = gps.readline()
     gps_sentences_fields = gps_sentece.split(",")
     #FILTRO DE LA SENTENCIA $GPRMC
@@ -47,45 +55,76 @@ while instruccion == 'y':
             lat = -lat
         if  gps_sentences_fields[6] == "W":
             longi = -longi
+#-----------Elección de region----------------------------
+        if region == 3:
+            region =2
+
+
+#-----------Elección de region----------------------------
 #-----------Calculo de distancia usando Harversine----------------------------
-        latref =9.02318033
-        longref = -79.53151733 #region1
+        if region == 1 :
+            #Region 1
+            latref =9.02318033
+            longref = -79.53151733
+            radius = 6371 # km
+            dlat = math.radians(latref-lat)
+            dlon = math.radians(longref-longi)
+            a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat)) \
+            * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            d = int((radius*c)*1000)
+            print d
+#-----------Calculo de distancia usando Harversine----------------------------
+        if region == 2 :
+        #Region 2
+        latref2=9.023149167
+        lonref2=-79.53156583
         radius = 6371 # km
-        dlat = math.radians(latref-lat)
-        dlon = math.radians(longref-longi)
+        dlat = math.radians(latref2-lat)
+        dlon = math.radians(longref2-longi)
         a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat)) \
-        * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+        * math.cos(math.radians(latref2)) * math.sin(dlon/2) * math.sin(dlon/2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
         d = int((radius*c)*1000)
         print d
 #------------------------------------------------------------------------------
 #--------comando = instrucion enviada al arduino---------------------------
 #----Los limites se definen mediante las coordenadas de referencias-------
-	if latref == 9.02318033 and longref == -79.53151733 :
-        #Limites para las region1
-		min1 = 34
-		max1 = 6  # min<--------------Region--------------------------->max
-		min2 = 6
-		#max2 =25
-		print("Region 1")
-	if latref == 9.023149167 and longref == -79.53156583 :
-        #Limite para Region 2
-		min1 = 7
-		max1 = 2 # min<--------------Region--------------------------->max
-		min2 = 1
-		#max2 = 8
-		print("Region 2")
-        if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
-               #arduino.write(Forward) #Mandar un comando hacia Arduino
-               bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
-               bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
-               print("Moviendo")
+	   if latref == 9.02318033 and longref == -79.53151733 :
+            #Limites para las region1
+    		min1 = 34
+    		max1 = 6  # min<--------------Region1--------------------------->max
+    		min2 = 6
+    		#max2 =25
 
-        if d <= min2:#Establece cuando curvara
-               #arduino.write(Turn)#Mandar un comando hacia Arduino
-               bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
-               bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
-               print("Curvand")
-            #time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
-               latref=9.023149167 #Cambio de la referencia para la region2
-               lonref=-79.53156583
+    		print("Region 1")
+
+
+	   if latref2 == 9.023149167 and longref2 == -79.53156583 :
+            #Limite para Region 2
+    		min1 = 7
+    		max1 = 2 # min<--------------Region2--------------------------->max
+    		min3 = 1
+    		#max2 = 8
+    		print("Region 2")
+
+       if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
+            #arduino.write(Forward) #Mandar un comando hacia Arduino
+            bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
+            bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
+            print("Moviendo")
+
+       if d <= min2:#Establece cuando curvara
+            #arduino.write(Turn)#Mandar un comando hacia Arduino
+            bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
+            bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
+            print("Curvando")
+            time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
+            region2=2
+       if d <= min3:#Establece cuando curvara
+            #arduino.write(Turn)#Mandar un comando hacia Arduino
+            bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
+            bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
+            print("Detener")
+            bus.write_byte(slaveAddress2, Stop)#Mandar un comando hacia MotorDerecho
+            bus.write_byte(slaveAddress1, Stop)#Mandar un comando hacia MotorIzquierdo
