45347564aca16a3c5a96a15156b07bb7dd6c9f0b
wggm01 <wggm01@gmail.com>
2018-03-08 22:34:25 -0500

    ultimos cambios

--- i2crpi.py
+++ i2crpi.py
@@ -1,11 +1,11 @@
 import smbus
 import time
-slaveAddress2 = 0x40
+#slaveAddress2 = 0x40
 slaveAddress1 = 0x50
 bus = smbus.SMBus(1)
 
 def writeNumber(value):
-    bus.write_byte(slaveAddress2, value) #Funcion
+   # bus.write_byte(slaveAddress2, value) #Funcion
     bus.write_byte(slaveAddress1, value)
    # bus.write_byte_data(address, 0, value)
     return -1
