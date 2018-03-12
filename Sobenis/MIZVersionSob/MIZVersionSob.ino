5afb426a0ab77d47380fb0b02dd2e9fa55d25280
wggm01 <37003923+wggm01@users.noreply.github.com>
2018-03-12 15:34:35 -0500

    Update MIZVersionSob.ino

--- Sobenis/MIZVersionSob/MIZVersionSob.ino
+++ Sobenis/MIZVersionSob/MIZVersionSob.ino
@@ -1,14 +1,10 @@
-//1-hacia alfrente
-//2-hacia atras
-//3-Dobla hacia la izquierda
-//4-Dobla hacia la derecha
-//5-Detener
+
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x50 //Direccion i2c del motor izquierda
 /*---------------------------------------------------------------------------------*/
 int control;
 int delay2 =1;
-int a;
+int a, b;
 /*---------------------------------------------------------------------------------*/
 //MotorIzquierdo
 int aPinm2; //INA 8
@@ -128,7 +124,13 @@ void Mov_Mizq (){
   digitalWrite(aPrimePinm2, LOW);
   digitalWrite(bPrimePinm2, LOW);
   }
+//Recepción de Datos provenientes de la raspberry pi3 model B  Rev 2
+void receiveEvent(int howMany) {
 
+if (Wire.available()==1) { // loop through all but the last
+  control = Wire.read();
+
+  }}
 /*---------------------------------------------------------------------------------*/
 //Logica de movimiento
 void loop() {
@@ -148,24 +150,42 @@ while(control == 1){
       digitalWrite(LED_BUILTIN,LOW); //Hacia alfrente
       delay(1000);*/
       if(control==2){break;}
-      if(control==3){break;}
-      if(control==4){break;}
-      if(control==5){break;}}
+     else if(control==3){break;}
+     else if(control==4){break;}
+     else if(control==5){break;}
+     else if(control==6){break;}
+     else if(control==7){break;}
+     else if(control==8){break;}}
 /*---------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------*/
 if(control == 2){
-  for(int i=0; i<400;){
+b=2;
+  for(;;){ 
       aPinm2 = 11; //INC 11
       bPinm2 = 9; //IND 9
       aPrimePinm2 = 10; //INB 10
       bPrimePinm2 = 8; //INA 8
      
-      if (control == 2){Mov_Mizq (); i = i+1;}
-      else if (control == 1){control = 1; i = i+400;}
-      else if (control == 4){control = 4; i = i+400;}
-      else if (control == 3){control = 3; i = i+400;}
-      else if (control == 5){control = 5; i = i+400;}
-
+      switch (b){
+        case 1 : control = 1;
+        break;
+        case 2 : Mov_Mizq ();
+        b= control;
+        break;
+        case 3 : control = 3;
+        break;
+        case 4 : control = 4; 
+        break;
+        case 5 : control = 5;
+        break;
+        case 6 : control = 6;
+        break;
+        case 7 : control = 7;
+        break;
+        default : control = 8;
+        break;
+      }
+ return 0;
       //digitalWrite(LED_BUILTIN,HIGH);
       //delay(500);
       //digitalWrite(LED_BUILTIN,LOW); //Hacia atras
@@ -174,29 +194,47 @@ if(control == 2){
       delay(500);
       digitalWrite(LED_BUILTIN,LOW); //Hacia atras
       delay(500);*/
-    } 
-      if (control == 1){control = 1;}
-      else if (control == 4){control = 4;}
-      else if (control == 3){control = 3;}
-      else if (control == 5){control = 5;}
-      else {control = 6;}}
+    }control =8; }
 /*---------------------------------------------------------------------------------*/
-    if(control == 3){
+   else if(control == 3){
       Stop_Mizq(); //Doblar hacia la izquierda
       control = 5;
  }
 /*---------------------------------------------------------------------------------*/
-   if(control == 4){
-for (int i=0; i<400;){
+   else if(control == 4){ 
+a =4;
+for (int i=0; i<400;){  // 210 para una vuelta de rueda
       aPinm2 = 8; //INA 8
       bPinm2 = 10; //INB 10
       aPrimePinm2 = 9; //IND 9
       bPrimePinm2 = 11; //INC 11
-      if (control == 4){Mov_Mizq (); i=i+1;}
-      if (control == 1){control = 1; i = i+400;}
-      if (control == 2){control = 2; i = i+400;}
-      if (control == 3){control = 3; i = i+400;}
-      if (control == 5){control = 5; i = i+400;}
+  
+      switch (control){
+        case 1 : control = 1;
+        i = i+400;
+        break;
+        case 2: control = 2;
+        i = i+400;
+        break;
+        case 3 : control = 3;
+        i = i+400;
+        break;
+        case 4 : Mov_Mizq ();
+        
+        i = i+1;
+        break;
+        case 5 : control = 5;
+        i = i+400;
+        break;
+        case 7 : control = 7;
+        i = i+400;
+        break;
+        case 8 : control = 8;
+        i = i+400;
+        break;
+        default : control = 8;
+        break;
+      }
     //digitalWrite(LED_BUILTIN,HIGH);
     //delay(100);
     //digitalWrite(LED_BUILTIN,LOW);//Dobla hacia la derecha
@@ -205,22 +243,33 @@ for (int i=0; i<400;){
     delay(100);
     digitalWrite(LED_BUILTIN,LOW);//Dobla hacia la derecha
     delay(100);*/
-
-
-  }}
+} control =8; }
 /*---------------------------------------------------------------------------------*/
- if(control == 5){
+ else if(control == 5){
       Stop_Mizq(); //Doblar hacia la izquierda
-      control = 6;
+      control = 8;
  }
 /*---------------------------------------------------------------------------------*/
-} //Fin de la logica de movimiento
-
-/*---------------------------------------------------------------------------------*/
-//Recepción de Datos provenientes de la raspberry pi3 model B  Rev 2
-void receiveEvent(int howMany) {
-
-if (Wire.available()==1) { // loop through all but the last
-  control = Wire.read();
-
+ else if (control == 6){
+  for (int i=0; i<400;){
+      aPinm2 = 8; //INA 8   // Turbo Giro derecha
+      bPinm2 = 10; //INB 10
+      aPrimePinm2 = 9; //IND 9
+      bPrimePinm2 = 11; //INC 11}
+      Mov_Mizq ();
+      i = i+1;
   }}
+/*---------------------------------------------------------------------------------*/
+ else if (control == 7){
+   for (int i=0; i<400;){
+     aPinm2 = 11; //INC 11 // Turbo Giro izquierda
+     bPinm2 = 9; //IND 9
+     aPrimePinm2 = 10; //INB 10
+     bPrimePinm2 = 8; //INA 8
+    Mov_Mizq ();
+    i = i+1;
+    }
+ }
+ 
+/*---------------------------------------------------------------------------------*/
+}//Fin de la logica de movimiento
