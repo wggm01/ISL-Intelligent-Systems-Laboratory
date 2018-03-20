ad4e35dec2f33db1c0566e1d7a767a7cb9da7989
JaySobe <37233739+JaySobe@users.noreply.github.com>
2018-03-20 15:05:02 -0500

    adecuaciones
    limpieza de historial de boton presionado y delay en el loop

--- Wallicontrol/dualshock3walli.py
+++ Wallicontrol/dualshock3walli.py
@@ -1,4 +1,5 @@
 import smbus
+import time
 from triangula.input import SixAxisResource, SixAxis
 slaveAddress2 = 0x40 #MotorIzquierdo
 slaveAddress1 = 0x50 #MotorDerecho
@@ -9,30 +10,37 @@ with SixAxisResource() as joystick:
         bus.write_byte(slaveAddress2, 1)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 1)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i Hacia adelante")
+        SixAxis.get_and_clear_button_press_history()
     def mov_b(button):
         bus.write_byte(slaveAddress2, 2)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 2)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i Hacia atras")
+        SixAxis.get_and_clear_button_press_history()
     def mov_tr(button):
         bus.write_byte(slaveAddress2, 8)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 8)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i derecha")
+        SixAxis.get_and_clear_button_press_history()
     def mov_tl(button):
         bus.write_byte(slaveAddress2, 9)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 9)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i izquierda")
+        SixAxis.get_and_clear_button_press_history()
     def mov_sr(button):
         bus.write_byte(slaveAddress2, 6)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 6)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i esta rotando hacia la derecha")
+        SixAxis.get_and_clear_button_press_history()
     def mov_sl(button):
         bus.write_byte(slaveAddress2, 7)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 7)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i acutalmente esta rotando hacia la izquierda")
+        SixAxis.get_and_clear_button_press_history()
     def mov_s(button):
         bus.write_byte(slaveAddress2, 5)#Mandar un comando hacia MotorDerecho
         bus.write_byte(slaveAddress1, 5)#Mandar un comando hacia MotorIzquierdo
         print("Wall-i me detuve")
+        SixAxis.get_and_clear_button_press_history()
 
     # Register the handler to the SQUARE button
     joystick.register_button_handler(mov_f, SixAxis.BUTTON_D_UP)
@@ -45,4 +53,5 @@ with SixAxisResource() as joystick:
     while 1:
         # Do stuff here, only register the button handlers once, not in this loop!
         # If the buttons are pressed, your handlers will be called but not from this thread.
+        time.sleep(3)
         pass
