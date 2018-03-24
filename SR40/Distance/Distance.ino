d4aa3ddf81166ef8a15e0e5fb233669ecb7887ea
wggm01 <wggm01@gmail.com>
2018-03-24 15:01:15 -0500

    nuevos diagrama de flujo

--- SR40/Distance/Distance.ino
+++ SR40/Distance/Distance.ino
@@ -4,7 +4,7 @@
 //Primer sensor (costado)
 #define TRIGGER_PIN 3 //pwm
 #define ECHO_PIN 4
-#define MAX_DISTANCE 200
+#define MAX_DISTANCE 300
 //Segundo sensor (costado)
 #define TRIGGER1_PIN 5
 #define ECHO1_PIN 7
@@ -30,9 +30,9 @@ const unsigned long period = 1000;  //the value is a number of milliseconds
 int control;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonar1(TRIGGER1_PIN, ECHO1_PIN, MAX1_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonar2(TRIGGER2_PIN, ECHO2_PIN, MAX2_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonar3(TRIGGER3_PIN, ECHO3_PIN, MAX3_DISTANCE); // NewPing setup of pins and maximum distance.
+//NewPing sonar1(TRIGGER1_PIN, ECHO1_PIN, MAX1_DISTANCE); // NewPing setup of pins and maximum distance.
+//NewPing sonar2(TRIGGER2_PIN, ECHO2_PIN, MAX2_DISTANCE); // NewPing setup of pins and maximum distance.
+//NewPing sonar3(TRIGGER3_PIN, ECHO3_PIN, MAX3_DISTANCE); // NewPing setup of pins and maximum distance.
  
 void setup() {
    Wire.begin(0X60);                
@@ -40,27 +40,43 @@ void setup() {
    Serial.begin(9600);
    startMillis = millis();  //initial start time
 }
+
+
  
 void loop() {
   currentMillis = millis();
   if (currentMillis - startMillis >= period)  //test whether the period has elapsed
     {
     uS1 = sonar.ping_cm();
-    uS2 = sonar1.ping_cm();
-    uS3 = sonar2.ping_cm();
-    uS4 = sonar3.ping_cm();
+    //uS2 = sonar1.ping_cm();
+    //uS3 = sonar2.ping_cm();
+    //uS4 = sonar3.ping_cm();
 
-  if (uS1 != 0 && uS2 != 0 && uS3 != 0 && uS4 != 0  )
+ /* if (uS1 != 0 && uS2 != 0 && uS3 != 0 && uS4 != 0  )
     {
       Serial.print(uS1);
       Serial.print(uS2);
       Serial.print(uS3);
       Serial.print(uS4);
       Serial.println("cm");
-    }   
+    } */  
   startMillis = currentMillis;
 }
-  //Logica de control aqui
+//Logica de control aqui
+/*Si el sensor de alfrente detecta cambio de distancia significa que esta cerca de una cera
+ * por lo que debe reajustarse rodando hacia atras luego gira a la izquierda sobre su eje 
+ * queda de frente, avanza,vuelve hacer un giro de 90 grados, chequea la distancia y si no 
+ * hay obstaculos ni una sigue hacia delante pero si se vuelve a topar con la acera tiene que
+ * repetir la secuencia antes mencionada.
+ */
+
+ //Entro en la verificacion de distancia
+  if (uS1>100 && uS1<190){ //si me topo con un desnivel tomando en cuenta que el sensor esta puesto en la parte superior del robot
+    control = 9; //Esto se lo envio a la raspberry para que haga la secuencia de correccion.
+  }else if (uS1>=200){
+    control = 8; 
+  }
+   
 }
 
 // Envio de data al masta
--- Wallicontrol/wallicontrolupdate.py
+++ Wallicontrol/wallicontrolupdate.py
@@ -1,17 +1,18 @@
 
 import serial
-import sys
+import sys #no utilizada aun
 import time
 import machinarie
-import smbus
+#import smbus
 region2=0
+checkD2 = 0
 #Lectura del puerto serial
 
 #gps = serial.Serial("/dev/ttyACM0", baudrate = 4800)
 
 """gps = serial.Serial("/dev/ttyACM0", baudrate = 4800)"""
 
-#gps = serial.Serial('COM14', 4800)
+"""#gps = serial.Serial('COM14', 4800)
 delay = 5 #Este valor hay que estimarlo al ojo.
 Forward=1
 Turn= 4
@@ -22,63 +23,35 @@ slaveAddress1 = 0x50 #MotorDerecho
 bus = smbus.SMBus(1) #Bus por el cual se comunican"""
 
 print("Wall-i actualmente se encuentra esperando su instruccion")
-instruccion = raw_input("Presion y luego enter para empezar\n ")
+instruccion = raw_input("Presion ""y"" y luego enter para empezar\n ")
 #loop para que muestre cada lectura que recibe el gps
 while instruccion == 'y':
     region = 1 + region2
+    checkD = 1 + checkD2
     if region == 3:
-            region =2
+        region = 2
+    if checkD == 3:
+        checkD = 2
     #velo=machinarie.veloWalli()
     #if velo != None:
      #   print ("La velocidad de Wall-i es de :",velo/1000,"m/s")
     data = machinarie.Data()
     if data!= None :
         latitud,longitud = data
-        #print (latitud)
-        #print (longitud)
-        #reg1=machinarie.distReg1(latitud,longitud)
-        #print(reg1)
-        #reg2=machinarie.distReg2(latitud,longitud)
-        #print(reg2)
         if region==1:
             d=machinarie.distReg1(latitud,longitud)
             #d=machinarie.distReg1_v(latitud,longitud)
-            #d=machinarie.distReg1_pi(latitud,longitud)
-            #drp = machinarie.angVariant(latitud,longitud,d)
             print("Wall-i esta a:",d," m De su objetivo")
-            #print("Distancia a modelo:",drp)
-            min1=200
-            max1=2
-            min2=2
-            if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
-                #arduino.write(Forward) #Mandar un comando hacia Arduino
-                bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
-                bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
-                print("Wall-i acutalmente se esta moviendo")
-
-            if d <= min2 :#Establece cuando curvara
-                #arduino.write(Turn)#Mandar un comando hacia Arduino
-                bus.write_byte(slaveAddress2, Turn)#Mandar un comando hacia MotorDerecho
-                bus.write_byte(slaveAddress1, Turn)#Mandar un comando hacia MotorIzquierdo
-                print("Wall-i actualmente esta curvando")
-                time.sleep(delay) #tiempo que demora en hacer un giro de 90 grados aprox
-                region2=2
-
+            machinarie.region1Bounds(d)
+           
         if region == 2:
             d=machinarie.distReg2(latitud,longitud)
             #d=machinarie.distReg2_v(latitud,longitud)
-            #d=machinarie.distReg2_pi(latitud,longitud)
             print("Wall-i esta a:",d,"m De su objetivo")
-            min1=200
-            max1=2
-            min3=2
-            if d < min1 and d >= max1: #Establece hasta donde se movera en linea recta
-                    #arduino.write(Forward) #Mandar un comando hacia Arduino
-                bus.write_byte(slaveAddress2, Forward)#Mandar un comando hacia MotorDerecho
-                bus.write_byte(slaveAddress1, Forward)#Mandar un comando hacia MotorIzquierdo
-                print("Wall-i acutalmente se esta moviendo")
-
-            if d <= min3:#Establece cuando curvara
-                print("Wall-i actualmente esta curvando")
-                bus.write_byte(slaveAddress2, Stop)#Mandar un comando hacia MotorDerecho
-                bus.write_byte(slaveAddress1, Stop)#Mandar un comando hacia MotorIzquierdo
+            machinarie.region2Bounds(d)
+            while checkD == 1:
+               machinarie.secCorrec()
+               if machinarie.secCorrec()== 2:
+                   checkD2=2
+                   break;
+            
