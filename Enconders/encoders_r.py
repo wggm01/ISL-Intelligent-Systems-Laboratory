216f80e77022349a2894803f87ff612c5f9962d5
wggm01 <wggm01@gmail.com>
2018-04-24 01:22:12 -0500

    enco simple

--- Enconders/encoders_r.py
+++ Enconders/encoders_r.py
@@ -1,11 +1,19 @@
+from time import sleep
 ticks = 0;
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-button1=27
-button2=22
-GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-while(1):
-    if GPIO.input(button1)== 0 and GPIO.input(button2)== 0 :
-        ticks = ticks + 1
-        print( "ticks")
+GPIO.setmode(GPIO.BCM)
+button1=12
+#button2=22
+GPIO.setup(button1,GPIO.IN)
+#GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+sensor = GPIO.input(button1)
+
+while True:
+	
+	sensor=GPIO.input(button1)
+	
+	if sensor == 1 :
+		ticks = ticks + 1
+		print( ticks )
+		sleep(.5)
+		
--- Wallicontrol/distance.py
+++ Wallicontrol/distance.py
@@ -1,69 +1,20 @@
-
 import smbus
-import time
+
 slaveAddress2 = 0x40
 slaveAddress1 = 0x50
+slave= 0x60
 bus = smbus.SMBus(1)
 
-#def writeNumber(value):
- #   bus.write_byte(slaveAddress2, value) #Funcion
-  #  bus.write_byte(slaveAddress1, value)
-   # bus.write_byte_data(address, 0, value)
-   # return -1
-#def readnumber():
- #   bus.read_byte(0X60)
-  #  return -1
+def get():
+     enco = bus.read_byte(slave)
+     return enco
+      
 
 while True:
-#    var = input("")
- #   if not var:
-  #      continue
-     if bus.read_byte(0X60) == 1:
-	bus.write_byte(slaveAddress1,1)
+
+     if get() == 2:
+	#bus.write_byte(slaveAddress1,1)
 	bus.write_byte(slaveAddress2,1)
-	print(bus.read_byte(0X40))
-     if bus.read_byte(0X60) == 7:
-	bus.write_byte(slaveAddress1,7)
-	bus.write_byte(slaveAddress2,7)
-    	print(bus.read_byte(0X50))
-
-     if bus.read_byte(0X60) == 8:
-	bus.write_byte(slaveAddress1,8)
-	bus.write_byte(slaveAddress2,8)
-	print(bus.read_byte(0X60))
- #   writeNumber(var)
-
-import smbus
-import time
-slaveAddress2 = 0x40
-slaveAddress1 = 0x50
-bus = smbus.SMBus(1)
-
-#def writeNumber(value):
- #   bus.write_byte(slaveAddress2, value) #Funcion
-  #  bus.write_byte(slaveAddress1, value)
-   # bus.write_byte_data(address, 0, value)
-   # return -1
-#def readnumber():
- #   bus.read_byte(0X60)
-  #  return -1
-
-while True:
-#    var = input("")
- #   if not var:
-  #      continue
-     if bus.read_byte(0X60) == 1:
-	bus.write_byte(slaveAddress1,1)
-	bus.write_byte(slaveAddress2,1)
-	print("Drive")
-     if bus.read_byte(0X60) == 2:
-	bus.write_byte(slaveAddress1,4)
-	bus.write_byte(slaveAddress2,4)
-    	print("Reverse")
-
-     if bus.read_byte(0X60) == 7:
-	bus.write_byte(slaveAddress1,7)
-	bus.write_byte(slaveAddress2,7)
-	print("Turn")
- #   writeNumber(var)
-
+     elif get() == 5:
+        #bus.write_byte(slaveAddress1,5)
+        bus.write_byte(slaveAddress2,5)
