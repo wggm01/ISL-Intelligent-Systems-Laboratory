7fa9a946c1bf5c2dc45da9b1e8ca915afb43fa8b
wggm01 <37003923+wggm01@users.noreply.github.com>
2018-05-05 00:51:54 -0500

    ultimos arreglos para probar codigo

--- Wallicontrol/Walk-e.py
+++ Wallicontrol/Walk-e.py
@@ -8,7 +8,7 @@ pnconfig.publish_key = "pub-c-b4c2e4df-fb98-4907-85f7-8e6392a93e48"
 pnconfig.ssl = False
 pubnub = PubNub(pnconfig)
 
-limit = 0
+limit = 2
 #Ciclo repetido
 print("Wall-i actualmente se encuentra esperando su instruccion")
 instruccion = raw_input("Presion ""y"" y luego enter para empezar\n ")
@@ -19,31 +19,48 @@ if data!= None :
     latitud,longitud = data   #Revision de posicion acual sin procesarself.
     envelope = pubnub.publish().channel("map2-channel").message({
     'lat': float(latitud),'lng': float(longitud)}).sync()
-    region = machinarie.check_drp(latitud,longitud)
-    reg0,reg1,reg2,reg3= region #Separacion de las regiones
-
+    
+    virtual_0 = machinarie.virtual_pos0()
+    latv0,lonv0= virtual_0
+    
+    virtual_1 = machinarie.virtual_pos1()
+    latv1,lonv1= virtual_1
+    
+    virtual_2 = machinarie.virtual_pos2()
+    latv2,lonv2= virtual_2
+    
+    virtual_3 = machinarie.virtual_pos3()
+    latv3,lonv3= virtual_3
+    
+    reg0 = machinarie.check_0drp(latitud,longitud,latv0,lonv0)
+    reg1 = machinarie.check_0drp(latitud,longitud,latv1,lonv1)
+    reg2 = machinarie.check_0drp(latitud,longitud,latv2,lonv2)
+    reg3 = machinarie.check_0drp(latitud,longitud,latv3,lonv3)
+    
     if  reg0 <= limit:
         #codigo
-        virtual = machinarie.virtual_pos0()
-        latv,lonv= virtual
-        d=machinarie.distReg0(latv,longv)
+        #virtual = machinarie.virtual_pos0()
+        #latv,lonv= virtual
+        d=machinarie.distReg0(latv0,longv0)
         machinarie.region0Bounds(d,reg0)
 
     elif reg1 <= limit:
         #codigo
-        virtual = machinarie.virtual_pos1()
-        latv,longv= virtual
-        d=machinarie.distReg1(latv,longv)
+        #virtual = machinarie.virtual_pos1()
+        #latv,longv= virtual
+        d=machinarie.distReg1(latv1,longv1)
         machinarie.region1Bounds(d,reg1)
+        
     elif reg2 <= limit:
         #codigo
-        virtual = machinarie.virtual_pos2()
-        latv,longv= virtual
-        d=machinarie.distReg2(latv,longv)
+        #virtual = machinarie.virtual_pos2()
+        #latv,longv= virtual
+        d=machinarie.distReg2(latv2,longv2)
         machinarie.region2Bounds(d,reg2)
+        
     elif reg3 <= limit;
         #codigo
-        virtual = machinarie.virtual_pos3()
-        latv,longv= virtual
-        d=machinarie.distReg3(latv,longv)
+        #virtual = machinarie.virtual_pos3()
+        #latv,longv= virtual
+        d=machinarie.distReg3(latv3,longv3)
         machinarie.region3Bounds(d,reg3)
--- Wallicontrol/machinarie.py
+++ Wallicontrol/machinarie.py
@@ -19,7 +19,7 @@ Turn= 4
 TurnLeftEje = 7
 Stop = 5
 delay = 5
-limit = 0
+limit = 2
 #Connecion al puerto serial
 #gps = serial.Serial('COM14', 4800)
 gps = serial.Serial("/dev/ttyACM0", baudrate = 4800)
@@ -28,8 +28,8 @@ gps = serial.Serial("/dev/ttyACM0", baudrate = 4800)
 
 #Vectores como variables globales incializdas en 0 para luego ser modi.
 p = [0,0] #Posicion dinamica
-xa = [0,0,0,0,] #X1 Y X2 corresponden a abcisas del punto inicio y finalsel.
-ya = [0,0,0,0] #Y1 Y Y2 corresponden a ordenadas del punto inicio y finalsel.
+xa = [-79.53169299,-79.53152138,-79.53177739,-79.53184155] #X1 Y X2 corresponden a abcisas del punto inicio y finalsel.
+ya = [9.023410836,9.023175628,9.022897331,9.023147334] #Y1 Y Y2 corresponden a ordenadas del punto inicio y finalsel.
 q = [0,0] # Puntos virtual trasladado
 drp = [0,0,0,0]#una para cada modelo y0,y1,y2,y3
 
@@ -72,13 +72,8 @@ def Data():
 
 #Calculo de de distancia para cada region (cambiar referencia en cada uno)
 def distReg0(latitud,longitud):
-    #Region 1 original 9.02318033 -79.53151733 (no forma parte del mapeo hecho orignalmente)
-    #Promedio  9.0232113 -79.5315376 (Si forma parte del mapeo)
-    #Pruebas alrededor de casa 9.04525 -79.40719
-    #latref =9.04525 #Casa
-    #longref = -79.40719
-    latref =9.02318033
-    longref = -79.53151733 #UTP
+    latref =9.023175628
+    longref = -79.53152138 #UTP
     radius = 6371 # km
     dlat = math.radians(latref-latitud)
     dlon = math.radians(longref-longitud)
@@ -88,18 +83,13 @@ def distReg0(latitud,longitud):
     d_raw = float((radius*c)*1000)
     d = int(d_raw)
     re= "Region 1"
-    with open ("logreg1.csv", "a") as pos:
+    with open ("logreg0.csv", "a") as pos:
         pos.write("%s, %s, %s, %s\n" % ( latitud, longitud, d, re ))
     return d
 
 def distReg1(latitud,longitud):
-    #Region 2 orignal 9.023149167 -79.53156583 (demasiado de cerca de region 1)
-    #Promedio 9.0230422 -79.5316507
-    #Pruebas 9.04485 -79.40695 alrededor casa
-    #latref2=9.04485 #casa
-    #lonref2=-79.40695
-    latref2=9.0230422 #UTP
-    lonref2=-79.5316507
+    latref2=9.022897331#UTP
+    lonref2=-79.53177739
     radius = 6371 # km
     dlat = math.radians(latref2-latitud)
     dlon = math.radians(lonref2-longitud)
@@ -109,18 +99,13 @@ def distReg1(latitud,longitud):
     d_raw = float((radius*c)*1000)
     d = int(d_raw)
     re= "Region 2"
-    with open ("logreg2.csv", "a") as pos:
+    with open ("logreg1.csv", "a") as pos:
         pos.write("%s, %s, %s, %s\n" % ( latitud, longitud, d, re ))
     return d
 
 def distReg2(latitud,longitud):
-    #Region 1 original 9.02318033 -79.53151733 (no forma parte del mapeo hecho orignalmente)
-    #Promedio  9.0232113 -79.5315376 (Si forma parte del mapeo)
-    #Pruebas alrededor de casa 9.04525 -79.40719
-    #latref =9.04525 #Casa
-    #longref = -79.40719
-    latref =9.02318033
-    longref = -79.53151733 #UTP
+    latref =9.023147334
+    longref = -79.53184155 #UTP
     radius = 6371 # km
     dlat = math.radians(latref-latitud)
     dlon = math.radians(longref-longitud)
@@ -130,18 +115,13 @@ def distReg2(latitud,longitud):
     d_raw = float((radius*c)*1000)
     d = int(d_raw)
     re= "Region 1"
-    with open ("logreg1.csv", "a") as pos:
+    with open ("logreg2.csv", "a") as pos:
         pos.write("%s, %s, %s, %s\n" % ( latitud, longitud, d, re ))
     return d
 
 def distReg3(latitud,longitud):
-    #Region 2 orignal 9.023149167 -79.53156583 (demasiado de cerca de region 1)
-    #Promedio 9.0230422 -79.5316507
-    #Pruebas 9.04485 -79.40695 alrededor casa
-    #latref2=9.04485 #casa
-    #lonref2=-79.40695
-    latref2=9.0230422 #UTP
-    lonref2=-79.5316507
+    latref2=9.023410836 #UTP
+    lonref2=-79.53169299
     radius = 6371 # km
     dlat = math.radians(latref2-latitud)
     dlon = math.radians(lonref2-longitud)
@@ -151,7 +131,7 @@ def distReg3(latitud,longitud):
     d_raw = float((radius*c)*1000)
     d = int(d_raw)
     re= "Region 2"
-    with open ("logreg2.csv", "a") as pos:
+    with open ("logreg3.csv", "a") as pos:
         pos.write("%s, %s, %s, %s\n" % ( latitud, longitud, d, re ))
     return d
 
@@ -215,40 +195,40 @@ def region3Bounds(d,reg3):
 
 #Traslacion de coordenadas
 def virtual_pos0 (latitud,longitud):
-    p[0]=latitud
-    p[1]=longitud
+    longitud=p[0]#x
+    latitud=p[1]
     #Calculo de pendientes:
     m1 =(ya[1]-ya[0])/(xa[1]-xa[0])
     m2 = -1/m1
     #calculo de interseccion de ambas rectas,esto me da la lat,long (virtual)
     x = (m1*xa[0]-ya[0]+p[1]-m2*p[0])/(m1-m2)
     y = m1*(x-xa[0])+ya[0]
-    q[0]=x#Latitud
-    q[1]=y#Longitud
+    q[0]=x#Longitud
+    q[1]=y#Latitud
     #calculo de distancia entre punto virtual trasladado y punto de referenciaself.
     #d_rest = math.sqrt(math.pow(xa[1]-q[0],2)+ math.pow(ya[1]-q[1],2))
 
-    return q[0],q[1]
+    return q[1],q[0]
 
 def virtual_pos1 (latitud,longitud):
-    p[0]=latitud
-    p[1]=longitud
+    longitud=p[0] #x
+    latitud=p[1]
     #Calculo de pendientes:
     m1 =(ya[1]-ya[0])/(xa[1]-xa[0])
     m2 = -1/m1
     #calculo de interseccion de ambas rectas,esto me da la lat,long (virtual)
     x = (m1*xa[0]-ya[0]+p[1]-m2*p[0])/(m1-m2)
     y = m1*(x-xa[0])+ya[0]
-    q[0]=x#Latitud
-    q[1]=y#Longitud
+    q[0]=x#Longitud
+    q[1]=y#Latitud
     #calculo de distancia entre punto virtual trasladado y punto de referenciaself.
     #d_rest = math.sqrt(math.pow(xa[1]-q[0],2)+ math.pow(ya[1]-q[1],2))
 
-    return q[0],q[1]
+    return q[1],q[0]
 
 def virtual_pos2 (latitud,longitud):
-    p[0]=latitud
-    p[1]=longitud
+    longitud=p[0] #x
+    latitud=p[1]
     #Calculo de pendientes:
     m1 =(ya[1]-ya[0])/(xa[1]-xa[0])
     m2 = -1/m1
@@ -260,11 +240,11 @@ def virtual_pos2 (latitud,longitud):
     #calculo de distancia entre punto virtual trasladado y punto de referenciaself.
     #d_rest = math.sqrt(math.pow(xa[1]-q[0],2)+ math.pow(ya[1]-q[1],2))
 
-    return q[0],q[1]
+    return q[1],q[0]
 
 def virtual_pos3 (latitud,longitud):
-    p[0]=latitud
-    p[1]=longitud
+    longitud=p[0] #x
+    latitud=p[1]
     #Calculo de pendientes:
     m1 =(ya[1]-ya[0])/(xa[1]-xa[0])
     m2 = -1/m1
@@ -276,16 +256,72 @@ def virtual_pos3 (latitud,longitud):
     #calculo de distancia entre punto virtual trasladado y punto de referenciaself.
     #d_rest = math.sqrt(math.pow(xa[1]-q[0],2)+ math.pow(ya[1]-q[1],2))
 
-    return q[0],q[1]
+    return q[1],q[0]
 
 #Chequeo de punto actual del robot a recta
-def check_drp (latitud,longitud):
+def check_0drp (latitud,longitud,latv0,lonv0):
     #Calculo de distancia a los modelos
-    #    drp[0]= #y0
-    #    drp[1]= #y1
-    #    drp[2]= #y2
-    #    drp[3]= #y3
-    return drp[0],drp[1],drp[2],drp[3]
+    latref = latv0
+    longref = lonv0
+    radius = 6371 # km
+    dlat = math.radians(latref-latitud)
+    dlon = math.radians(longref-longitud)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(latitud)) \
+    * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d_raw = float((radius*c)*1000)
+    d = int(d_raw)
+    d = drp[0]
+    
+    return drp[0]
+
+def check_1drp (latitud,longitud,latv1,lonv1):
+    #Calculo de distancia a los modelos
+    latref = latv1
+    longref = lonv1
+    radius = 6371 # km
+    dlat = math.radians(latref-latitud)
+    dlon = math.radians(longref-longitud)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(latitud)) \
+    * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d_raw = float((radius*c)*1000)
+    d = int(d_raw)
+    d = drp[1]
+    
+    return drp[1]
+
+def check_2drp (latitud,longitud,latv2,lonv2):
+    #Calculo de distancia a los modelos
+    latref = latv2
+    longref = lonv2
+    radius = 6371 # km
+    dlat = math.radians(latref-latitud)
+    dlon = math.radians(longref-longitud)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(latitud)) \
+    * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d_raw = float((radius*c)*1000)
+    d = int(d_raw)
+    d = drp[2]
+    
+    return drp[2]
+
+def check_3drp (latitud,longitud,latv3,lonv3):
+    #Calculo de distancia a los modelos
+    latref = latv3
+    longref = lonv3
+    radius = 6371 # km
+    dlat = math.radians(latref-latitud)
+    dlon = math.radians(longref-longitud)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(latitud)) \
+    * math.cos(math.radians(latref)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d_raw = float((radius*c)*1000)
+    d = int(d_raw)
+    d = drp[3]
+  
+    return drp[3]
 
 
 
