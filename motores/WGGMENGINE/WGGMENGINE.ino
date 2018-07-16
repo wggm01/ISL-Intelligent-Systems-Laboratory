bc8cd43f09efac71d33fd864496445640f0e170d
wggm01 <wggm01@gmail.com>
2018-07-16 16:03:59 -0500

    mejoras

--- motores/WGGMENGINE/WGGMENGINE.ino
+++ motores/WGGMENGINE/WGGMENGINE.ino
@@ -1,8 +1,8 @@
 #include <Stepper.h>
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x40
-const int stepsPerRevolution = 827;  //OBTENIDO POR EXPERIMENTACION
-char control;
+const int stepsPerRevolution = 200;  //OBTENIDO POR EXPERIMENTACION
+int control;
 
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11); //AJUSTAR A LOS MOTORES
 
@@ -11,6 +11,7 @@ void setup() {
   Wire.begin(SLAVE_ADDRESS); 
   Wire.onReceive(receiveEvent); 
   Serial.begin(9600);
+  
 }
 //RECEPCION DE INSTRUCCIONES
 void receiveEvent(int howMany) {
@@ -21,44 +22,14 @@ if (Wire.available()>0) {
 } 
 //MIZQ
 void loop() {
-  myStepper.setSpeed(velo); //VELOCIDAD MAS BAJA
-  switch (b){
-        case "w" : 
+  myStepper.setSpeed(150);
+  switch (control){
+        case 1 : 
           myStepper.step(stepsPerRevolution); //HACIA DELANTE
+          Serial.println("F");
         break;
-        case "s" : 
+        case 2 : 
           myStepper.step(-stepsPerRevolution); //HACIA ATRAS
+          Serial.println("B");
         break;
-        case "a" :
-          myStepper.step(stepsPerRevolution);//IZQ
-        break;
-        case "d" : 
-          myStepper.step(0);//DER SOBRE LLANTA OPUESTA
-        break;
-        case "q" : 
-          myStepper.step(stepsPerRevolution);//ACKERMAN IZQ
-          myStepper.setSpeed(velo+10); //AJUSTE NECESARIO
-        break;
-        case "e" : 
-          myStepper.step(stepsPerRevolution);//ACKERMAN DER
-          myStepper.setSpeed(velo-10); //AJUSTE NECESARIO
-        break;
-        case "z" : 
-          myStepper.step(-stepsPerRevolution);//ACKERMAN IZQ HACIA ATRAS
-          myStepper.setSpeed(velo+10); //AJUSTE NECESARIO
-        break;
-        case "c": 
-          myStepper.step(-stepsPerRevolution);//ACKERMAN DER HACIA ATRAS
-          myStepper.setSpeed(velo-10); //AJUSTE NECESARIO
-        break;
-        case "x" : 
-          myStepper.step(0);
-        break;
-        case "o" : 
-          myStepper.step(-stepsPerRevolution);
-        break;
-        case "p" : 
-          myStepper.step(stepsPerRevolution);
-        break;}
-  
-}
+       }}
