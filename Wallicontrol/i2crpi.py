3df810ab9701ad94cbd469c4cdae93e4fa908edc
wggm01 <37003923+wggm01@users.noreply.github.com>
2018-07-15 23:34:08 -0500

    arreglo para pruebas

--- Wallicontrol/i2crpi.py
+++ Wallicontrol/i2crpi.py
@@ -1,28 +1,23 @@
-import smbus
+import smbus2
+from smbus2 import SMBus
+from smbus2 import SMBusWrapper
 import time
-slaveAddress2 = 0x40
-slaveAddress1 = 0x50
+mizq = 0x40
+mder = 0x50
 bus = smbus.SMBus(1)
 
-def writeNumber(value):
-    bus.write_byte(slaveAddress2, value) #Funcion
-    bus.write_byte(slaveAddress1, value)
-   # bus.write_byte_data(address, 0, value)
-    return -1
-#def readnumber():
- #   bus.read_byte(0X60)
-  #  return -1
+def wr_i2c (instruction):
+    with SMBusWrapper(1) as bus:
+    # Write a byte to address 80, offset 0
+        data = instruction  
+        bus.write_byte_data(mizq, 0, instruction)
+        bus.write_byte_data(mder, 0, instruction)
 
 while True:
-    var = input("")
-    if not var:
-        continue
-    # if bus.read_byte(0X60) == 1:
-#	bus.write_byte(slaveAddress1,1)
-#	bus.write_byte(slaveAddress2,1)
-#	print("F")
- #    if bus.read_byte(0X60) == 4:
-#	bus.write_byte(slaveAddress1,4)
-#	bus.write_byte(slaveAddress2,4)
- #   	print("Derecha")
-    writeNumber(var)
+    instruction = raw_input("Enter the data to be sent : ")
+	data_list = list(instruction)
+	for i in data_list:
+    	#Sends to the Slaves 
+        wr_i2c(int(ord(i)))
+        time.sleep(.1)
+  
