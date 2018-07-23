8d1bc3e2f3173ddc2409282a75198618d1da24fd
wggm01 <37003923+wggm01@users.noreply.github.com>
2018-07-22 22:36:23 -0500

    pruebas a servo remoto

--- Wallicontrol/servo.py
+++ Wallicontrol/servo.py
@@ -1,6 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+#RECEPCION DE PAQUETES POR UDP
+import socket, traceback
+from struct import *
+host = '192.168.0.247'
+port = 5556
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
 
+#CONTROL DE SERVO CON LIBRERIA RPI
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
@@ -11,11 +21,21 @@ def angle(ang):
 	duty=1./18.*(ang)+1
 	if duty > 11:
 		duty=7
-	return duty 
+	return duty
+
+def mapazimut(ang_raw,in_min,in_max,out_min,out_max):
+	angle=(ang_raw-in_min)*(out_max-out_min)/(in_max-in_min)+out_min
+	return angle 
 try:
   while True:
-    angulo=input("angulo pliz")
-    desireang=angle(angulo)
+    data, address = s.recvfrom(8192)
+    
+    azimut= unpack_from ('!f', data, 36)
+    
+    azimap_raw=azimut[0]
+    azimap=int(azimap_raw)
+    desireang=mapazimut(azimap,-127,77,30,180)
+    print (desireang,azimut)
     p.ChangeDutyCycle(desireang)
     time.sleep(0.1)
 	
