38a1d9cb125c9822503c702990817c9f7e672bbf
wggm01 <37003923+wggm01@users.noreply.github.com>
2018-09-06 22:47:44 -0500

    arreglos para pruebas
    TENGO QUE HACER LA FUNCION EN MACHINARIE Y ACTUALIZAR EL REPOSITORIO. QUE NO SE ME OLVIDE!!!

--- Wallicontrol/Walk-e.py
+++ Wallicontrol/Walk-e.py
@@ -2,7 +2,6 @@
 #VERSION: RETO TOKIO
 import os  #modulo para interactuar con el sistema
 import subprocess
-from gps import* #modulo para trabajar con el gpsd
 import time
 import  threading
 from time import* #modulo para los retrasos
@@ -28,139 +27,106 @@ os.system('clear') #clear the terminal (optional)
 os.system('i2cdetect -y 1') #clear the terminal (optional)
 #os.system('cgps') #clear the terminal (optional)
 #os.system('sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock')
-class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
+try:
 
-  def run(self):
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+    print("El Programa empezara al recibir la confirmacion por parte del gps")
 
-if __name__ == '__main__':
-	gpsp = GpsPoller() # create the thread
-	try:
-		gpsp.start()
-		print("Revise todo antes de comenzar")
-		instruccion = raw_input("Presion ""y"" y luego enter para empezar\n ")
-		while instruccion == 'y':
+    while machinarie.gps_check() == 1:
 
-    #data = machinarie.Data()
-    #if data!= None :
-    #    global latitud
-    #    global longitud
-    #    latitud,longitud = data   #Revision de posicion acual sin procesarself.
+        data = machinarie.Data()
+
+        if data!= None :
+            latitud,longitud = data   #Revision de posicion acual sin procesarself.
+
         #envelope = pubnub.publish().channel("map2-channel").message({
         #'lat': float(latitud),'lng': float(longitud)}).sync()
 			#data de gps sin tratar
-			global latitud
-			global longitud
-			latitud_raw= gpsd.fix.latitude
-			longitud_raw= gpsd.fix.longitude
-			time= datetime.datetime.now()
-			
-			with open ("raw_coords.csv", "a") as pos:
-                                pos.write("%s, %s, %s \n" % ( latitud_raw, longitud_raw,time ))
+		global latitud
+		global longitud
 
-			machinarie.get_data_gyro()
-			machinarie.get_data_ultra()
+		latitud_raw= latitud
+		longitud_raw= longitud
+		time= datetime.datetime.now()
 
-			nrp = machinarie.not_repeatcoord(latitud_raw,longitud_raw)
-			if nrp != None:
-				latitud,longitud = nrp
+		with open ("raw_coords.csv", "a") as pos:
+            pos.write("%s, %s, %s \n" % ( latitud_raw, longitud_raw,time ))
 
-			virtual_0 = machinarie.virtual_pos0(latitud,longitud)
-			global latv0
-			global lonv0
-			latv0,lonv0= virtual_0
+		machinarie.get_data_gyro()
+		machinarie.get_data_ultra()
 
-			virtual_1 = machinarie.virtual_pos1(latitud,longitud)
-			global latv1
-			global lonv1
-			latv1,lonv1= virtual_1
+		nrp = machinarie.not_repeatcoord(latitud_raw,longitud_raw) #EVITA LA REPETICION DE COORDENADAS
+		      if nrp != None:
+                  latitud,longitud = nrp
 
-			virtual_2 = machinarie.virtual_pos2(latitud,longitud)
-			global latv2
-			global lonv2
-			latv2,lonv2= virtual_2
+		virtual_0 = machinarie.virtual_pos0(latitud,longitud)
+		global latv0
+		global lonv0
+		latv0,lonv0= virtual_0
 
-			virtual_3 = machinarie.virtual_pos3(latitud,longitud)
-			global latv3
-			global lonv3
-			latv3,lonv3= virtual_3
+		virtual_1 = machinarie.virtual_pos1(latitud,longitud)
+		global latv1
+		global lonv1
+		latv1,lonv1= virtual_1
 
-			with open ("Virtual_pos.csv", "a") as pos:
-				pos.write("%s, %s, %s, %s, %s, %s, %s, %s\n" % ( latv0, lonv0, latv1, lonv1, latv2, lonv2, latv3, lonv3 ))
+		virtual_2 = machinarie.virtual_pos2(latitud,longitud)
+		global latv2
+		global lonv2
+		latv2,lonv2= virtual_2
 
-			reg0 = machinarie.check_0drp(latitud,longitud,latv0,lonv0)
-			reg1 = machinarie.check_1drp(latitud,longitud,latv1,lonv1)
-			reg2 = machinarie.check_2drp(latitud,longitud,latv2,lonv2)
-			reg3 = machinarie.check_3drp(latitud,longitud,latv3,lonv3)
-			print("DRP0",reg0,"DRP1",reg1,"DRP2",reg2,"DRP3",reg3)
+		virtual_3 = machinarie.virtual_pos3(latitud,longitud)
+		global latv3
+		global lonv3
+		latv3,lonv3= virtual_3
 
-			with open ("drp.csv", "a") as pos:
-				pos.write("%s, %s, %s, %s\n" % ( reg0,reg1,reg2,reg3))
+		with open ("Virtual_pos.csv", "a") as pos:
+            pos.write("%s, %s, %s, %s, %s, %s, %s, %s\n" % ( latv0, lonv0, latv1, lonv1, latv2, lonv2, latv3, lonv3 ))
 
-			if  reg0 <= limit:
-            #codigo
-            #virtual = machinarie.virtual_pos0()
-            #latv0,lonv0= virtual
-				d=machinarie.distReg0(latv0,lonv0)
-            #print(d)
-				with open ("d0.csv", "a") as pos:
-					pos.write("%s\n" % (d))
+		reg0 = machinarie.check_0drp(latitud,longitud,latv0,lonv0)
+		reg1 = machinarie.check_1drp(latitud,longitud,latv1,lonv1)
+		reg2 = machinarie.check_2drp(latitud,longitud,latv2,lonv2)
+		reg3 = machinarie.check_3drp(latitud,longitud,latv3,lonv3)
 
+		print("DRP0",reg0,"DRP1",reg1,"DRP2",reg2,"DRP3",reg3)
 
+		with open ("drp.csv", "a") as pos:
+            pos.write("%s, %s, %s, %s\n" % ( reg0,reg1,reg2,reg3))
 
-				machinarie.region0Bounds(d,reg0)
+		if  reg0 <= limit:
+            d=machinarie.distReg0(latv0,lonv0)
+            with open ("d0.csv", "a") as pos:
+                pos.write("%s\n" % (d))
 
-			elif reg1 <= limit:
-            #codigo
-            #virtual = machinarie.virtual_pos1()
-            #latv,longv= virtual
-				d=machinarie.distReg1(latv1,lonv1)
+            machinarie.region0Bounds(d,reg0)
 
-				with open ("d1.csv", "a") as pos:
-					pos.write("%s\n" % (d))
+		elif reg1 <= limit:
+            d=machinarie.distReg1(latv1,lonv1)
 
+			with open ("d1.csv", "a") as pos:
+                pos.write("%s\n" % (d))
 
-				machinarie.region1Bounds(d,reg1)
+            machinarie.region1Bounds(d,reg1)
 
-			elif reg2 <= limit:
-            #codigo
-            #virtual = machinarie.virtual_pos2()
-            #latv,longv= virtual
-				d=machinarie.distReg2(latv2,lonv2)
+		elif reg2 <= limit:
+            d=machinarie.distReg2(latv2,lonv2)
 
-				with open ("d2.csv", "a") as pos:
-					pos.write("%s\n" % (d))
+            with open ("d2.csv", "a") as pos:
+                pos.write("%s\n" % (d))
 
+            machinarie.region2Bounds(d,reg2)
 
+		elif reg3 <= limit:
 
-				machinarie.region2Bounds(d,reg2)
+            d=machinarie.distReg3(latv3,lonv3)
 
-			elif reg3 <= limit:
-            #codigo
-            #virtual = machinarie.virtual_pos3()
-            #latv,longv= virtual
-				d=machinarie.distReg3(latv3,lonv3)
+            with open ("d3.csv", "a") as pos:
+                pos.write("%s\n" % (d))
 
-				with open ("d3.csv", "a") as pos:
-					pos.write("%s\n" % (d))
+            machinarie.region3Bounds(d,reg3)
 
+		else:
+            print("No se donde estoy")
 
+except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    machinarie.hardrst(int(20))
 
-				machinarie.region3Bounds(d,reg3)
-			else:
-				print("No se donde estoy")
-
-	except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-		machinarie.hardrst(int(20))
-		gps.running = False
-		gps.join()
-		print "Cerrando programa, bye :D"
-	
+    print "Cerrando programa, bye :D"
